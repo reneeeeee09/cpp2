575c6d1816b0a82bada79071d496f8c6141a55f1
reneeeeeee09 <cursosdecomputacion.net@gmail.com>
2023-09-27 09:02:07 -0900

    agregando ejercicio 2

---
+++ ejercicios/ejercicio 2/mensajes .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    cout<<"ingrese su nombre :";
+  cin >> nombre;
+
+cout << endl;
+  cout<<"hola mi nombre es " << nombre;
+  cout << endl;
+  
+    return 0;
+}
